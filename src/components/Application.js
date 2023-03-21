e43721ca8862cc6d771bf1da52e788b99556808e
viacaelestis <affectuminflamma@gmail.com>
2023-03-21 04:25:28 -0600

    move useState to within functional component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
   return (
     <main className="layout">
       <section className="sidebar">
@@ -31,10 +32,10 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu"> 
-        <DayList
+        <DayList 
           days={days}
-          day={"Monday"}
-          setDay={day => console.log(day)}
+          day={day}
+          setDay={setDay}
         />
         </nav>
         <img
