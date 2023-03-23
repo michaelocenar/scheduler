d97a52ea689e111fa8765f49e0a9024890356c5c
viacaelestis <affectuminflamma@gmail.com>
2023-03-22 23:22:08 -0600

    create error and form component

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,8 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -167,5 +169,11 @@ storiesOf("Appointment", module)
   .add("Status", () => (
     <Status
       message="Deleting"
+    />
+  ))
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
     />
   ));
