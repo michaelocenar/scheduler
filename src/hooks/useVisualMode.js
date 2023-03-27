4a80389db5d9a4d23d985e39cd21d03132918d65
michaelocenar <affectuminflamma@gmail.com>
2023-03-27 17:37:01 -0600

    build hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      const newHistory = [...history.slice(0, -1), newMode];
+      setHistory(newHistory);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
+    setMode(newMode);
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = history.slice(0, -1);
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  };
+
+  return {
+    mode,
+    transition,
+    back,
+  };
+}
