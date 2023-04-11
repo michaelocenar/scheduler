f3b533e0239cdfbfb6cbcbad0d7fa7d11c1bb977
michaelocenar <affectuminflamma@gmail.com>
2023-04-11 05:12:50 -0600

    remove localhost 8001 from axios requests

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,9 +12,9 @@ export default function useApplicationData() {
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
 
   useEffect(() => {
-    const getDays = axios.get("http://localhost:8001/api/days");
-    const getAppointments = axios.get("http://localhost:8001/api/appointments");
-    const getInterviewers = axios.get("http://localhost:8001/api/interviewers");
+    const getDays = axios.get("/api/days");
+    const getAppointments = axios.get("/api/appointments");
+    const getInterviewers = axios.get("/api/interviewers");
   
     Promise.all([getDays, getAppointments, getInterviewers])
       .then((all) => {
